89d556e9fa8f68754a6b910dbd625bc46706b2a2
Vladimir <rvladimir001@gmail.com>
2021-11-27 16:57:51 +0300

    Revert "сконфигурирован router" This reverts commit 0060c910

--- src/App.vue
+++ src/App.vue
@@ -4,69 +4,98 @@
   </header>
   <div class="controls">
     <div
-      v-for="link in linksNames"
-      :key="link.id"
+      v-for="component in componentsNames"
+      :key="component.id"
       class="controls-btn"
-      :class="{ active: link.class }"
-      @click="selectComponent(link.link)"
+      :class="{ active: component.class }"
+      @click="selectComponent(component.component)"
     >
-      <router-link :to="{ path: `${link.link}` }">{{ link.name }}</router-link>
+      {{ component.name }}
     </div>
   </div>
   <router-view />
+  <component
+    :is="name"
+    :dataList="basicData"
+    @addBooks="addNewBook"
+    @addAuthors="addNewAuthors"
+  ></component>
 </template>
 
 <script>
-import { useStore } from "vuex";
-import { computed, onMounted, ref } from "vue";
-
+import Books from "./components/Books.vue";
+import Authors from "./components/Authors.vue";
+import BooksForm from "./components/BooksForm.vue";
+import AuthorsForm from "./components/AuthorsForm.vue";
+import { addMockData, addMockPoster } from "./mock.js";
+import { onMounted, ref } from "vue";
+import axios from "axios";
 export default {
   name: "App",
+  components: {
+    Books,
+    Authors,
+    AuthorsForm,
+    BooksForm,
+  },
   setup() {
-    const store = useStore();
     const basicData = ref([]);
     let name = ref("Books");
-    const linksNames = ref([
+    const componentsNames = ref([
       {
         id: 1,
-        link: "Books",
+        component: "Books",
         name: "Книги",
         class: true,
       },
       {
         id: 2,
-        link: "Authors",
+        component: "Authors",
         name: "Авторы",
         class: false,
       },
       {
         id: 3,
-        link: "addbooks",
+        component: "BooksForm",
         name: "Добавить книгу",
         class: false,
       },
       {
         id: 4,
-        link: "addauthors",
+        component: "AuthorsForm",
         name: "Добавить автора",
         class: false,
       },
     ]);
-    onMounted(() => {
-      store.dispatch("getBooks");
-    });
-    const data = computed(() => store.state.basicData);
-    const selectComponent = (link) => {
-      for (const item of linksNames.value) {
-        item.class = item.link === link;
+    onMounted(() => getBooks());
+    const getBooks = () => {
+      axios.get("https://gutendex.com/books/?page=1").then((response) => {
+        const result = response.data.results;
+        basicData.value = addMockData(result).map((book) =>
+          Object.assign({}, book)
+        );
+      });
+    };
+    const addNewBook = (book) => {
+      basicData.value.unshift(addMockPoster(book.value));
+    };
+    const addNewAuthors = (author) => {
+      basicData.value.unshift(author.value);
+    };
+    const selectComponent = (component) => {
+      name.value = component;
+      for (const item of componentsNames.value) {
+        item.class = item.component === component;
       }
     };
     return {
+      getBooks,
       basicData,
       name,
-      linksNames,
+      componentsNames,
       selectComponent,
-      data,
+      addNewBook,
+      addNewAuthors,
     };
   },
 };
@@ -76,7 +105,6 @@ export default {
 html {
   height: 100%;
 }
-
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -85,12 +113,10 @@ html {
   color: #2c3e50;
   height: 100%;
 }
-
 body {
   margin: 0 !important;
   height: 100%;
 }
-
 .controls {
   display: flex;
   justify-content: space-around;
@@ -98,84 +124,64 @@ body {
   height: 40px;
   align-items: center;
 }
-
 .controls-btn {
   border-bottom: 1px solid #dae2e4;
 }
-
 .controls-btn:hover {
   border-bottom: 1px solid #000;
   cursor: pointer;
 }
-
-.controls-btn a {
-  color: #000000;
-  text-decoration: none;
-}
-
 .active {
   font-weight: bold;
 }
-
 header {
   background: linear-gradient(90deg, #a64fc5, #4f54e6);
   text-align: left;
   padding: 20px;
 }
-
 h1 {
   color: #fff;
 }
-
 table {
   width: 90%;
   margin: 0 auto;
   border-collapse: collapse;
   color: #000;
 }
-
 th {
   border-bottom: 2px solid #a9a9a9;
   padding: 10px;
   text-align: center;
 }
-
 td {
   padding: 10px;
 }
-
 tr:nth-child(odd) {
   background: white;
 }
-
 tr:nth-child(even) {
   background: #dae2e4;
 }
-
 ul li {
   margin: 10px;
   list-style-type: none;
 }
-
 ul {
   margin-left: 0;
   padding-left: 0;
 }
-
 ul li input,
 ul li textarea {
   width: 350px;
   border: 1px solid #dae2e4;
   padding: 5px;
 }
-
 ul li textarea {
   max-width: 350px;
   min-width: 350px;
   min-height: 100px;
   max-height: 100px;
 }
-
 ul li input:active,
 ul li input:hover,
 ul li input:focus,
@@ -185,7 +191,6 @@ ul li textarea:focus {
   outline: 0;
   outline-offset: 0;
 }
-
 .btn {
   width: 100px;
   height: 20px;
@@ -194,19 +199,15 @@ ul li textarea:focus {
   padding: 10px;
   border-radius: 3px;
 }
-
 .btn:hover {
   box-shadow: inset 0px 0px 16px 13px rgba(355, 355, 355, 0.3);
 }
-
 .btn:active {
   box-shadow: inset 0px 0px 6px 3px rgba(0, 0, 0, 0.3);
 }
-
 .no-data {
   padding: 30px;
 }
-
 .control-panel {
   display: flex;
   justify-content: space-around;
